3eede5fdc9472245f5eb26c26f944f04760dc3ad
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-26 19:12:15 +0530

    REFACTORED : stored double flip coin counts

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,29 +6,62 @@ IS_HEAD=0
 IS_TAILS=1
 plays=0
 #VARIABLES
-declare -A singleCoin
+declare -A coin
 randomNumber=0
-singleCoin[H]=0
-singleCoin[T]=0
+randomNumber2=0
+coin[HH]=0
+coin[TT]=0
+coin[HT]=0
+coin[TH]=0
+coin[T]=0
+coin[H]=0
 
 printf "Enter How many times you want to flip the coin:\n"
 read plays
 
+#SINGLE COIN FLIP
 for (( index=1; index<=$plays; index++ ))
 do
 	randomNumber=$(( RANDOM % 2 ))
+
    if [ $randomNumber -eq $IS_HEAD ]
       then
-         singleCoin[H]=$(( "${singleCoin[H]}" + 1 ))
+         coin[H]=$(( "${coin[H]}" + 1 ))
       else
-         singleCoin[T]=$(( "${singleCoin[T]}" + 1 ))
+         coin[T]=$(( "${coin[T]}" + 1 ))
 fi
 done
 
-echo  ${singleCoin[@]}
+#DOUBLE COIN FLIP
 
-head=`expr "scale=3; ( ${singleCoin[H]} / $plays ) * 100" | bc -l`
-tails=`expr "scale=3; ( ${singleCoin[T]} / $plays ) * 100" | bc -l`
+for (( index=1; index<=$plays; index++ ))
+do
+   randomNumber=$(( RANDOM % 2 ))
+	randomNumber2=$(( RANDOM % 2 ))
+   if [ $randomNumber -eq $IS_HEAD -a $randomNumber2 -eq $IS_HEAD ]
+      then
+         coin[HH]=$(( "${coin[HH]}" + 1 ))
+      elif [ $randomNumber -eq $IS_HEAD -a $randomNumber2 -eq $IS_TAILS ]
+      	then
+				coin[HT]=$(( "${coin[HT]}" + 1 ))
+			elif [ $randomNumber -eq $IS_TAILS -a $randomNumber2 -eq $IS_HEAD ]
+				then
+					coin[TH]=$(( "${coin[TH]}" + 1 ))
+				elif [ $randomNumber -eq $IS_TAILS -a $randomNumber2 -eq $IS_TAILS ]
+					then
+						coin[TT]=$(( "${coin[TT]}" + 1 ))
+	fi
+done
 
-printf "head percentage: $head \ntails percantage: $tails \n"
+echo ${coin[@]}
+echo ${!coin[@]}
+HH=`expr "scale=3; ( ${coin[HH]} / $plays ) * 100" | bc -l`
+HT=`expr "scale=3; ( ${coin[HT]} / $plays ) * 100" | bc -l`
+TT=`expr "scale=3; ( ${coin[TT]} / $plays ) * 100" | bc -l`
+TH=`expr "scale=3; ( ${coin[TH]} / $plays ) * 100" | bc -l`
+H=`expr "scale=3; ( ${coin[H]} / $plays ) * 100" | bc -l`
+T=`expr "scale=3; ( ${coin[T]} / $plays ) * 100" | bc -l`
+printf "H percentage: $H \nT percantage: $T \n"
+printf "HH percentage: $HH \nHT percantage: $HT \nTT percentage: $TT\nTH percentage: $TH\n"
+
 
